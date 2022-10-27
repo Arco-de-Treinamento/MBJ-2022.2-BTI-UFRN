140e58d910e28ed21dea68d9a8397f76e255f3cd
Manoel Freitas <josmanoel.freitas@gmail.com>
2022-10-27 14:56:13 -0300

    Revert ":truck: chore: Renomeando e removendo arquivos"
    This reverts commit 2e48d8ce57e1d8b5450154983add276ff384a586, reversing
    changes made to 1f1f5c8bbafab5d2a5716c640f265cec68a88b9c.

--- UnityProject/Assets/Scripts/Generic_Enemy.cs
+++ UnityProject/Assets/Scripts/Generic_Enemy.cs
@@ -3,44 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Generic_Enemy : MonoBehaviour{
-    public int currentHealth;
-    private Transform player;
-    private Vector3 playerDistance;
-    public int maxHealth = 5;
-    public int touchingDamage;
+    public int lifeEnemy;
 
-    void Start(){
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        this.currentHealth = maxHealth;
-        this.touchingDamage = 35;
-    }
-
+    // Update is called once per frame
     void Update(){
-        isAlive();
-        playerDistance = player.transform.position - transform.position;
+        // Destroy o inimigo quando a vida chegar ao fim
+        if(lifeEnemy <= 0) Destroy(gameObject);
     }
 
-    private void isAlive(){
-        if(currentHealth <= 0){
-            //Adicionar animação de morte aqui
-            Destroy(gameObject);
-        }
-    }
-
+    // Funcao publica que diminui a vida do inimigo
     public void TakeDamage(int damage){ 
-        currentHealth -= damage;
+        lifeEnemy -= damage;
     }
-
-    //causa dano no player e joga ele para trás
-    private void OnCollisionEnter2D(Collision2D collision){
-        Player player = collision.gameObject.GetComponent<Player>();
-        if( player != null){
-            //causa dano de encostar no player
-            player.takeDamage(this.touchingDamage);
-
-            //Joga o player para trás
-            player.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 8 * (playerDistance.x / Mathf.Abs(playerDistance.x)),ForceMode2D.Impulse);
-        }
-    }
-
 }
